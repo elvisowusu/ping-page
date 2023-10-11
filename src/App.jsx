9feae7dff5f502a0fc6080cc81323ee503c6a5cc
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-11 17:21:40 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       
-    </>
+    </div>
   )
 }
 
