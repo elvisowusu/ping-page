110732f78eecac6dcc72189e67a7c0bc4ad505da
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-11 17:35:01 +0000

    added assests and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
+import logo from './assets/logo.svg'
+import dashboard from './assets/illustration-dashboard.png'
 import './App.css'
 
 function App() {
 
   return (
     <div>
-      
+      <img src={logo} alt="" />
+      <h1>We are launching <span>soon</span></h1>
+      <p>Subscribe and get notified</p>
+      <form >
+        <input 
+          placeholder='Your email address'
+          type="email"
+         />
+         <input className='bg-blue-600 text-white'
+          value="Notify Me"
+          type="submit"
+           />
+      </form>
+      <div>
+        <img src={dashboard} alt="" />
+      </div>
     </div>
   )
 }
