6be77e66578558d2b55783061f9de5cfb8182901
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-14 21:55:28 +0000

    added more styles

--- src/App.jsx
+++ src/App.jsx
@@ -15,28 +15,29 @@ function App() {
         alt="logo"
         className='h-4 mt-[4rem] mb-7'
       />
-      <p className='text-[1.3rem] text-colorGray'>We are launching <span className='font-black text-VeryDarkBlue' >soon!</span></p>
-      <p className='text-[0.7rem] mt-2 mb-6 font-medium'>Subscribe and get notified</p>
+      <p className='text-[1.3rem] text-colorGray'>We are launching <span className='font-extrabold text-VeryDarkBlue' >soon!</span></p>
+      <p className='text-[0.7rem] mt-2 mb-6 font-light'>Subscribe and get notified</p>
       <form className='flex flex-col'>
         <input
-        className='placeholder:text-sm placeholder:text-PaleBlue border border-PaleBlue rounded-2xl w-[16rem] h-9 px-4 mb-2 focus:border-Colorblue outline-none' 
+          className='placeholder:text-sm placeholder:text-PaleBlue border border-PaleBlue rounded-2xl w-[16rem] h-9 px-4 mb-2 focus:border-Colorblue outline-none' 
           placeholder='Your email address'
           type="email"
          />
-        <input className='bg-Colorblue text-white text-xs rounded-2xl h-9 font-semibold'
+        <input
+          className='bg-Colorblue text-white text-xs rounded-2xl h-9 font-semibold mb-[4rem]'
           value="Notify Me"
           type="submit"
-           />
+        />
       </form>
       <div>
         <img src={dashboard} alt="" />
       </div>
-      <div className='flex'>
+      <div className='flex gap-2'>
         <BiLogoFacebook className='rounded-full h-7 w-7 px-[0.2rem] border text-Colorblue border-blue-700 hover:bg-Colorblue hover:text-white'/>
         <BiLogoTwitter className='rounded-full h-7 w-7 px-[0.3rem] border text-Colorblue border-blue-700 hover:bg-Colorblue hover:text-white'/>
         <FiInstagram className='rounded-full h-7 w-7 px-[0.5rem] border text-Colorblue border-blue-700 hover:bg-Colorblue hover:text-white'/>
       </div>
-      <p className='text-[0.6rem]'>Copyright Ping. All rights reserved</p>
+      <p className='text-[0.6rem] text-colorGray'>© Copyright Ping. All rights reserved</p>
     </div>
   ) 
 }
