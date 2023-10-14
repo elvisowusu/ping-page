5d0f71066906886991d5fabcdb262156a139f255
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-14 18:12:45 +0000

    added styles to the icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { useState } from 'react'
 import logo from './assets/logo.svg'
 import dashboard from './assets/illustration-dashboard.png'
+import {BiLogoFacebook} from 'react-icons/bi'
+import {BiLogoTwitter} from 'react-icons/bi'
+import {FiInstagram} from 'react-icons/fi'
 import './App.css'
 
 function App() {
 
   return (
     <div className='flex flex-col justify-center items-center' >
-      <img src={logo} alt="" />
-      <h1>We are launching <span>soon</span></h1>
-      <p>Subscribe and get notified</p>
-      <form >
+      <img
+        src={logo}
+        alt="logo"
+        className='h-4 mt-[4rem] mb-7'
+      />
+      <p className='text-[1.45rem]'>We are launching <span className='' >soon</span></p>
+      <p className='text-sm'>Subscribe and get notified</p>
+      <form className='flex flex-col'>
         <input 
           placeholder='Your email address'
           type="email"
          />
-         <input className='bg-blue-600 text-white'
+        <input className='bg-blue-600 text-white'
           value="Notify Me"
           type="submit"
            />
@@ -23,12 +30,14 @@ function App() {
       <div>
         <img src={dashboard} alt="" />
       </div>
-      <div>
-
+      <div className='flex'>
+        <BiLogoFacebook className='rounded-full h-7 w-7 px-[0.4rem] border border-blue-700'/>
+        <BiLogoTwitter className='rounded-full h-7 w-7 px-[0.4rem]'/>
+        <FiInstagram className='rounded-full h-7 w-7 px-[0.5rem]'/>
       </div>
-      <p>Copyright Ping. All rights reserved</p>
+      <p className='text-[0.6rem]'>Copyright Ping. All rights reserved</p>
     </div>
-  )
+  ) 
 }
 
 export default App
