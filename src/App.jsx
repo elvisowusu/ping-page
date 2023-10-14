67d41281f67a267211b48f5c1c21617079ab0169
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-14 22:32:59 +0000

    binding the form

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,10 @@ import './App.css'
 
 function App() {
 
-const {register, handleSubmit,errors} = useForm()
-
+const {register, handleSubmit,errors} = useForm();
+  const onSubmit=(data)=>{
+    console.log(data);
+  }
   return (
     <div className='flex flex-col justify-center items-center font-LibreFranklin font-medium' >
       <img
@@ -20,16 +22,18 @@ const {register, handleSubmit,errors} = useForm()
       />
       <p className='text-[1.3rem] text-colorGray'>We are launching <span className='font-extrabold text-VeryDarkBlue' >soon!</span></p>
       <p className='text-[0.7rem] mt-2 mb-6 font-light text-VeryDarkBlue'>Subscribe and get notified</p>
-      <form className='flex flex-col'>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col'>
         <input
           className='placeholder:text-sm placeholder:text-PaleBlue border border-PaleBlue rounded-2xl w-[16rem] h-9 px-4 mb-2 focus:border-Colorblue outline-none' 
           placeholder='Your email address'
           type="email"
+          {...register("email")}
          />
         <input
           className='bg-Colorblue text-white text-xs rounded-2xl h-9 font-semibold mb-[4rem] shadow-md shadow-PaleBlue'
           value="Notify Me"
           type="submit"
+          
         />
       </form>
       <div>
