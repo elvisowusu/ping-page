c8bb944d1e5a0d0357e8650c4caa897414a26922
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-14 22:09:59 +0000

    added more styles including shadow

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         className='h-4 mt-[4rem] mb-7'
       />
       <p className='text-[1.3rem] text-colorGray'>We are launching <span className='font-extrabold text-VeryDarkBlue' >soon!</span></p>
-      <p className='text-[0.7rem] mt-2 mb-6 font-light'>Subscribe and get notified</p>
+      <p className='text-[0.7rem] mt-2 mb-6 font-light text-VeryDarkBlue'>Subscribe and get notified</p>
       <form className='flex flex-col'>
         <input
           className='placeholder:text-sm placeholder:text-PaleBlue border border-PaleBlue rounded-2xl w-[16rem] h-9 px-4 mb-2 focus:border-Colorblue outline-none' 
@@ -24,7 +24,7 @@ function App() {
           type="email"
          />
         <input
-          className='bg-Colorblue text-white text-xs rounded-2xl h-9 font-semibold mb-[4rem]'
+          className='bg-Colorblue text-white text-xs rounded-2xl h-9 font-semibold mb-[4rem] shadow-md shadow-PaleBlue'
           value="Notify Me"
           type="submit"
         />
@@ -32,12 +32,12 @@ function App() {
       <div>
         <img src={dashboard} alt="" />
       </div>
-      <div className='flex gap-2'>
-        <BiLogoFacebook className='rounded-full h-7 w-7 px-[0.2rem] border text-Colorblue border-blue-700 hover:bg-Colorblue hover:text-white'/>
-        <BiLogoTwitter className='rounded-full h-7 w-7 px-[0.3rem] border text-Colorblue border-blue-700 hover:bg-Colorblue hover:text-white'/>
-        <FiInstagram className='rounded-full h-7 w-7 px-[0.5rem] border text-Colorblue border-blue-700 hover:bg-Colorblue hover:text-white'/>
+      <div className='flex gap-2 mt-[6rem]'>
+        <BiLogoFacebook className='rounded-full h-7 w-7 px-[0.2rem] border text-Colorblue border-PaleBlue hover:bg-Colorblue hover:text-white'/>
+        <BiLogoTwitter className='rounded-full h-7 w-7 px-[0.3rem] border text-Colorblue border-PaleBlue hover:bg-Colorblue hover:text-white'/>
+        <FiInstagram className='rounded-full h-7 w-7 px-[0.5rem] border text-Colorblue border-PaleBlue hover:bg-Colorblue hover:text-white'/>
       </div>
-      <p className='text-[0.6rem] text-colorGray'>© Copyright Ping. All rights reserved</p>
+      <p className='text-[0.6rem] text-colorGray mt-7'>© Copyright Ping. All rights reserved</p>
     </div>
   ) 
 }
