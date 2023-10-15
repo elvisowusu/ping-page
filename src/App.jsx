8c364631c75e6c16b26d9948708addfd561c12ec
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-15 00:51:42 +0000

    done with sm: screen

--- src/App.jsx
+++ src/App.jsx
@@ -34,15 +34,18 @@ function App() {
       <p className='text-[1.3rem] sm:text-[1.7rem] text-colorGray'>We are launching <span className='font-extrabold text-VeryDarkBlue' >soon!</span></p>
       <p className='text-[0.7rem] sm:text-[0.8rem] mt-1 mb-6 font-light text-VeryDarkBlue'>Subscribe and get notified</p>
       <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col sm:flex-row sm:gap-1'>
+        <div>
+
         <input
-          className={`text-sm placeholder:text-sm placeholder:text-PaleBlue border border-PaleBlue rounded-3xl w-[16rem] h-9 px-4 focus:border-Colorblue ${errors.email? 'border-LightRed focus:border-LightRed placeholder:text-black placeholder:font-semibold':''} outline-none py-5`}
+          className={`text-sm placeholder:text-sm placeholder:text-PaleBlue border border-PaleBlue rounded-3xl w-[16rem] h-9 px-4 focus:border-Colorblue ${errors.email? 'border-LightRed focus:border-LightRed placeholder:text-black placeholder:font-semibold':''} outline-none sm:py-5 sm:px-5`}
           placeholder={`${errors.email? "example@email/com":"Your email address"}`}
           type="email"
           {...register("email")}
         />
-        <p className='text-xs mb-2 text-LightRed italic'>{errors.email?.message}</p>
+        <p className='text-xs mb-2 text-LightRed italic text-left pl-5'>{errors.email?.message}</p>
+        </div>
         <input
-          className='cursor-pointer bg-Colorblue text-white text-xs rounded-3xl h-9 font-semibold mb-[4rem] shadow-md shadow-PaleBlue hover:border hover:border-Colorblue hover:text-Colorblue hover:bg-white'
+          className='cursor-pointer bg-Colorblue text-white text-xs rounded-3xl h-9 sm:h-10 font-semibold mb-[4rem] shadow-md shadow-PaleBlue hover:border hover:border-Colorblue hover:text-Colorblue hover:bg-white sm:px-6 '
           value="Notify Me"
           type="submit"
         />
